01bb1fa98243858f239f61a437c964b8031fc540
Vivian Liao <vivian.liao@thoughtworks.com>
2019-04-17 16:36:02 -0700

    initial commit, clicking tile updates number

--- src/App.js
+++ src/App.js
@@ -1,27 +1,122 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import glen from './Glen.png';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      squares: Array(9).fill(null),
+    }
+  }
+
+  renderSquare() {
+    return <Square></Square>
+  }
+
+  handleClick(){
+    //get the value in each square, put in an array to check?
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <img src={glen} alt='glen'></img>
+        <div className="Board">
+          <div className="board-row">
+            {this.renderSquare()}
+            {/* {this.renderSquare()}
+            {this.renderSquare()} */}
+            </div>
+            {/* <div className="board-row">
+            {this.renderSquare()}
+            {this.renderSquare()}
+            {this.renderSquare()}
+          </div>
+          <div className="board-row">
+            {this.renderSquare()}
+            {this.renderSquare()}
+            {this.renderSquare()}
+          </div> */}
+        </div>
+        <button onClick={this.handleClick}>Check Answer</button>
       </div>
-    );
+    )
+  }
+}
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  renderTile() {
+    return <Tile />
+  }
+  // renderTile(i) {
+  //   return <Tile onClick={()=> this.onClick(i)} value={this.state.squares[i]}/>
+  // }
+  onClick(i){
+    console.log("clicked!");
+    const squares = this.state.squares.slice();
+    if (squares[i] < 9) {
+      squares[i] = squares[i] + 1 ;
+      this.setState({ squares : squares});
+    }
+    else {
+      this.setState({ value: null });
+    }
+  }
+
+  render() {
+    return (
+      <div className="subBoard">
+        <div className="subBoard-row">
+          {this.renderTile()}
+          {this.renderTile()}
+          {this.renderTile()}
+        </div>
+        <div className="subBoard-row">
+          {this.renderTile()}
+          {this.renderTile()}
+          {this.renderTile()}
+        </div>
+        <div className="subBoard-row">
+          {this.renderTile()}
+          {this.renderTile()}
+          {this.renderTile()}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("clicked")
+    if (this.state.value < 9) {
+      this.setState({ value: this.state.value + 1 });
+    }
+    else {
+      this.setState({ value: null });
+    }
+  }
+
+  render() {
+    return (
+      <button className="tile"
+        onClick={this.handleClick}> {this.state.value}</button>
+   //   on click update the value  //in the future, when something is dragged over.
+    )
   }
 }
 
